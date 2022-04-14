eb2777504aa0f71942abaa88f6d922685eee6d65
wu <527869722@qq.com>
2022-04-14 11:52:17 +0800

    feat: 226翻转二叉树

--- src/main/java/per/senawu/algorithm/leetcode/binarytree/Solution226.java
+++ src/main/java/per/senawu/algorithm/leetcode/binarytree/Solution226.java
@@ -39,4 +39,23 @@ public class Solution226 {
 
         return root;
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
