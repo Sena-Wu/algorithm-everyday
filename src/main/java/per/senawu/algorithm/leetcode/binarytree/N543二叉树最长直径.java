98d643f56c0285117bcecc563392b417d5a3cc4a
wu <527869722@qq.com>
2022-04-16 20:53:18 +0800

    feat:  N543二叉树最长直径

--- src/main/java/per/senawu/algorithm/leetcode/binarytree/N543二叉树最长直径.java
+++ src/main/java/per/senawu/algorithm/leetcode/binarytree/N543二叉树最长直径.java
@@ -8,22 +8,7 @@ package per.senawu.algorithm.leetcode.binarytree;
  * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
  */
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-public class Solution543 {
+public class N543二叉树最长直径 {
     int result=0;
     public int diameterOfBinaryTree(TreeNode root) {
         work(root);
