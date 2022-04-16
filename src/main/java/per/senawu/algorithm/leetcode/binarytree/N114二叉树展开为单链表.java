f93d152248be504d22203275e13277c7816ea81c
wu <527869722@qq.com>
2022-04-16 20:52:05 +0800

    feat:  N114二叉树展开为单链表

--- src/main/java/per/senawu/algorithm/leetcode/binarytree/N114二叉树展开为单链表.java
+++ src/main/java/per/senawu/algorithm/leetcode/binarytree/N114二叉树展开为单链表.java
@@ -7,13 +7,14 @@ package per.senawu.algorithm.leetcode.binarytree;
 
 /**
  *给你二叉树的根结点 root ，请你将它展开为一个单链表：
- *
- * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+ * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null
  * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ *
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/
  */
 
 
-public class Solution114 {
+public class N114二叉树展开为单链表 {
     TreeNode head =  new TreeNode();
     TreeNode head1 = head;
     public void flatten(TreeNode root) {
