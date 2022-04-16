97032215c2052fa9ac61dbca0892ae2826d3b29c
wu <527869722@qq.com>
2022-04-16 20:51:26 +0800

    feat:  N98搜索树合法性判断

--- src/main/java/per/senawu/algorithm/leetcode/binarytree/N98搜索树合法性判断.java
+++ src/main/java/per/senawu/algorithm/leetcode/binarytree/N98搜索树合法性判断.java
@@ -7,15 +7,16 @@ package per.senawu.algorithm.leetcode.binarytree;
 
 /**
  * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
- *
  * 有效 二叉搜索树定义如下：
  *      节点的左子树只包含 小于 当前节点的数。
  *      节点的右子树只包含 大于 当前节点的数。
  *      所有左子树和右子树自身必须也是二叉搜索树。
-
+ *
+ * https://leetcode-cn.com/problems/validate-binary-search-tree/
  */
-public class Solution98 {
+public class N98搜索树合法性判断 {
     public boolean isValidBST(TreeNode root) {
+
         return isValidBST(root, null, null);
     }
 
