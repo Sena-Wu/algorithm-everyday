609965676c6ce1e044ef2f40d9e7ef03cb2ec836
wu <527869722@qq.com>
2022-05-06 15:23:30 +0800

    feat: N528按权重随机选择

--- src/main/java/per/senawu/algorithm/leetcode/arr/N528按权重随机选择.java
+++ src/main/java/per/senawu/algorithm/leetcode/arr/N528按权重随机选择.java
@@ -4,12 +4,23 @@ package per.senawu.algorithm.leetcode.arr;
  * @author Sena-wu
  * @date 2022/5/6
  */
+
+/**
+ * 给你一个 下标从 0 开始 的正整数数组 w ，其中 w[i] 代表第 i 个下标的权重。
+ * 请你实现一个函数 pickIndex ，它可以 随机地 从范围 [0, w.length - 1] 内（含 0 和 w.length - 1）选出并返回一个下标。选取下标 i 的 概率 为 w[i] / sum(w) 。
+ *
+ * 链接：https://leetcode-cn.com/problems/random-pick-with-weight
+ */
+
+/**
+ * 前缀和 and 二分查找
+ */
 public class N528按权重随机选择 {
-    int count;
+    int count = 0;
     int[] nums;
     public N528按权重随机选择(int[] w) {
         nums = w;
-        count = 0;
+        // 计算前缀和数组
         for (int i = 0; i < w.length;i++){
             count += w[i];
             nums[i] = count;
@@ -17,7 +28,9 @@ public class N528按权重随机选择 {
     }
 
     public int pickIndex() {
-        int rand = (int) Math.random() * count;
+        // 取[1, count]内一个随机数
+        // Math.random() -> [0, 1);  Math.random() * count -> [0, count)
+        int rand = (int) Math.random() * count + 1;
         return findX(nums, rand);
     }
 
