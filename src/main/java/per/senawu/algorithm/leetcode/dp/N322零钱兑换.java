f7d9f8e1e8b221240865b747fc3705f2410a7a9f
wu <527869722@qq.com>
2022-07-14 13:14:45 +0800

    feat: N322零钱兑换

--- src/main/java/per/senawu/algorithm/leetcode/dp/N322零钱兑换.java
+++ src/main/java/per/senawu/algorithm/leetcode/dp/N322零钱兑换.java
@@ -1,5 +1,6 @@
 package per.senawu.algorithm.leetcode.dp;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -14,57 +15,84 @@ import java.util.HashMap;
  *
  * 链接：https://leetcode.cn/problems/coin-change
  */
-public class N322零钱兑换 {
-    private HashMap<String, Integer> dpTable = new HashMap<>();
-    private int size;
-    private int[] coins;
-    public int coinChange(int[] coins, int amount) {
-        size = coins.length;
-        this.coins = coins;
-        dp(amount, 0);
-        return dpTable.get("-"+amount+"-"+0);
 
+/**
+ * dp[i] = Math.min(dp[i] , dp[i - coins[j]] + 1)
+ * dp 初始化填充值 amount + 1; dp[0] = 0;
+ * 1 计算[1, amount]内各个金额最少需要几次
+ * 2 遍历coins: if (coins[j] <= target)
+ *      3 dp[target] = Math.min(dp[target] , dp[target - coins[j]] + 1)
+ *
+ * 4 return dp[amount] > amount ? -1 : dp[amount];
+ */
+public class N322零钱兑换 {
+
+    public int coinChange(int[] coins, int amount) {
+        // 最小面额至少为1, 最多也就只可能要 amount 个 1
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        // 计算[1, amount]内各个金额最少需要几次
+        for (int i = 1; i <= amount; i ++){
+            for (int j = 0; j < coins.length; j ++){
+                if (coins[j] <= i){
+                    dp[i] = Math.min(dp[i] , dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
-    void dp(int amount, int start){
-        if (amount == 0){
-            dpTable.put("-"+amount+"-"+start, 0);
-            return;
-        }
-        if (start >= size){
-            dpTable.put("-"+amount+"-"+start, -1);
-            return;
-        }
-        if (amount >= coins[start]){
-            Integer x = dpTable.get("-"+(amount - coins[start])+"-"+start);
-            Integer y = dpTable.get("-"+amount+"-"+(start + 1));
-            if (x  == null){
-                dp(amount - coins[start], start);
-                x = dpTable.get("-"+(amount - coins[start])+"-"+start);
-            }
-            if (y == null){
-                dp(amount, start + 1);
-                y = dpTable.get("-"+amount+"-"+(start + 1));
-            }
-            if (x == -1 && y == -1){
-                dpTable.put("-"+amount+"-"+start, -1);
-            }else if (x == -1){
-                dpTable.put("-"+amount+"-"+start, y);
-            }else if (y == -1){
-                dpTable.put("-"+amount+"-"+start, x + 1);
-            }else{
-                dpTable.put("-"+amount+"-"+start, Math.min(x + 1, y));
-            }
-        }else{
-            Integer y = dpTable.get("-"+amount+"-"+(start + 1));
-            if (y == null){
-                dp(amount, start + 1);
-                y = dpTable.get("-"+amount+"-"+(start + 1));
-            }
-            if (y == -1){
-                dpTable.put("-"+amount+"-"+start, -1);
-            }else{
-                dpTable.put("-"+amount+"-"+start, y);
-            }
-        }
-    }
+
+//    private HashMap<String, Integer> dpTable = new HashMap<>();
+//    private int size;
+//    private int[] coins;
+//    public int coinChange(int[] coins, int amount) {
+//        size = coins.length;
+//        this.coins = coins;
+//        dp(amount, 0);
+//        return dpTable.get("-"+amount+"-"+0);
+//    }
+//    void dp(int amount, int start){
+//        if (amount == 0){
+//            dpTable.put("-"+amount+"-"+start, 0);
+//            return;
+//        }
+//        if (start >= size){
+//            dpTable.put("-"+amount+"-"+start, -1);
+//            return;
+//        }
+//        if (amount >= coins[start]){
+//            Integer x = dpTable.get("-"+(amount - coins[start])+"-"+start);
+//            Integer y = dpTable.get("-"+amount+"-"+(start + 1));
+//            if (x  == null){
+//                dp(amount - coins[start], start);
+//                x = dpTable.get("-"+(amount - coins[start])+"-"+start);
+//            }
+//            if (y == null){
+//                dp(amount, start + 1);
+//                y = dpTable.get("-"+amount+"-"+(start + 1));
+//            }
+//            if (x == -1 && y == -1){
+//                dpTable.put("-"+amount+"-"+start, -1);
+//            }else if (x == -1){
+//                dpTable.put("-"+amount+"-"+start, y);
+//            }else if (y == -1){
+//                dpTable.put("-"+amount+"-"+start, x + 1);
+//            }else{
+//                dpTable.put("-"+amount+"-"+start, Math.min(x + 1, y));
+//            }
+//        }else{
+//            Integer y = dpTable.get("-"+amount+"-"+(start + 1));
+//            if (y == null){
+//                dp(amount, start + 1);
+//                y = dpTable.get("-"+amount+"-"+(start + 1));
+//            }
+//            if (y == -1){
+//                dpTable.put("-"+amount+"-"+start, -1);
+//            }else{
+//                dpTable.put("-"+amount+"-"+start, y);
+//            }
+//        }
+//    }
 }
